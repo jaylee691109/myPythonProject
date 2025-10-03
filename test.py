036161b9aaec987c0c52c6b6a94674e35eabeefd
myPythonProject <cv2000@naver.com>
2025-10-03 19:26:31 +0800

    첫 커밋

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-#test
+def gugu(dan):
+
+    result = []
+
+    for i in range(1, 10):   # 1~9까지
+
+        result.append(dan * i)
+
+    return result
+
+​
+
+print(gugu(2))  # [2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+​
