54930ff93aa674f5edfa1260fe3e5dc303788548
jaylee691109 <cv2000@naver.com>
2025-10-03 19:10:51 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# 회귀: 주택 크기 → 가격 (scikit-learn)
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
-X = np.array([[30],[50],[80],[120]])   # m²  
-y = np.array([200, 320, 520, 760])     # 가격(예시)
-model = LinearRegression().fit(X, y)
-print("예측(100m²):", model.predict([[100]])[0])
